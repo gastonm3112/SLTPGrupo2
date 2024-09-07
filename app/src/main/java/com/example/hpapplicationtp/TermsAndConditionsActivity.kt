c99658186372591cc7a66158868adda7cb264d44
Gaston Martinez <gastonm3112@gmail.com>
2024-09-07 15:40:12 -0300

    ADD: Login and Terms and Conditions Activities

--- app/src/main/java/com/example/hpapplicationtp/TermsAndConditionsActivity.kt
+++ app/src/main/java/com/example/hpapplicationtp/TermsAndConditionsActivity.kt
@@ -1,32 +1,34 @@
 package com.example.hpapplicationtp
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TermsAndConditionsActivity : AppCompatActivity() {
+    lateinit var btnAceptarTerminos: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_terms_and_conditions)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        saludarUsuario()
-    }
+        btnAceptarTerminos = findViewById(R.id.btnAceptarTerminos)
 
-    private fun saludarUsuario() {
-        val bundle: Bundle? = intent.extras
-
-        if (bundle != null){
-            val nombreUsuario = bundle?.getString("Nombre")
-            Toast.makeText(this, "Bienvenido/a $nombreUsuario", Toast.LENGTH_SHORT).show()
+        btnAceptarTerminos.setOnClickListener {
+            Log.i("TODO", "Se Aceptaron los terminos y condiciones")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
