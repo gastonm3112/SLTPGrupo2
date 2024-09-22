22acbd02b9143bbf7190cdd57cf29904a7e35f2a
Gaston Martinez <gastonm3112@gmail.com>
2024-09-22 15:33:14 -0300

    ADD: Registrar Usuario Activity guarda usuarios en ls DB local

--- app/src/main/java/com/example/hpapplicationtp/AppDatabase.kt
+++ app/src/main/java/com/example/hpapplicationtp/AppDatabase.kt
@@ -5,9 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Examen::class], version = 1)
+@Database(entities = [Usuario::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
-    abstract fun examenDao(): ExamenDao
+    abstract fun usuarioDao(): UsuarioDao
 
     companion object {
         private var INSTANCIA: AppDatabase? = null
