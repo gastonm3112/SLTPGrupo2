f83589cecc51cdf8cfbfa48c9e09a7366fc27667
Lettian <crislettieri92@gmail.com>
2024-10-19 18:39:20 -0300

    - Se implementa el back menu entre lista personajes y detalles personajes.

--- app/src/main/java/com/example/hpapplicationtp/CharacterDetailActivity.kt
+++ app/src/main/java/com/example/hpapplicationtp/CharacterDetailActivity.kt
@@ -1,19 +1,29 @@
 package com.example.hpapplicationtp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.hpapplicationtp.dtos.Post
 
 class CharacterDetailActivity : AppCompatActivity() {
 
     private lateinit var tvCharacterDetails: TextView
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_detail)
 
         tvCharacterDetails = findViewById(R.id.tvCharacterDetails)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+
 
         // Obtener el personaje del intent
         val character = intent.getParcelableExtra<Post>("character")
@@ -43,4 +53,20 @@ class CharacterDetailActivity : AppCompatActivity() {
             Alive: ${character.alive}
         """.trimIndent()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_back, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_volver){
+            var intent = Intent(this, ListHpCharacterActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
--- app/src/main/java/com/example/hpapplicationtp/ListHpCharacterActivity.kt
+++ app/src/main/java/com/example/hpapplicationtp/ListHpCharacterActivity.kt
@@ -2,8 +2,11 @@ package com.example.hpapplicationtp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hpapplicationtp.configurations.RetrofitClient
@@ -17,12 +20,19 @@ class ListHpCharacterActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var charactersAdapter: CharactersAdapter
 
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_hp_character)
 
         recyclerView = findViewById(R.id.recyclerViewCharacters)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+
 
         charactersAdapter = CharactersAdapter(emptyList()) { character ->
             // Este bloque se ejecuta cuando se hace clic en un personaje
@@ -62,4 +72,19 @@ class ListHpCharacterActivity : AppCompatActivity() {
     private fun showError(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_back, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_volver){
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
